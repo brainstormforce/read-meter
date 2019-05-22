0b0332c6530eed7c7b755a45f270167ebdc9206e
Rajkiran Gajanan Bagal <rajkiranb@bsf.io>
2019-05-22 12:01:46 -0700

    Added a User manual tab

--- includes/bsf-rt-main-frontend.php
+++ includes/bsf-rt-main-frontend.php
@@ -12,7 +12,10 @@ if (isset($_GET["tab"])) {
         $active_tab = "bsf_rt_read_time_settings";
     } elseif ($_GET["tab"] == "bsf_rt_progress_bar_settings") {
         $active_tab = "bsf_rt_progress_bar_settings";
+    } elseif ($_GET["tab"] == "bsf_rt_user_manual") {
+        $active_tab = "bsf_rt_user_manual";
     }
+
 }  
 ?>
 <!-- wordpress provides the styling for tabs. -->
@@ -28,6 +31,9 @@ if (isset($_GET["tab"])) {
             <a href="?page=bsf_rt&tab=bsf_rt_progress_bar_settings" class="nav-tab tb <?php if ($active_tab == 'bsf_rt_progress_bar_settings') {
                         echo 'nav-tab-active';
         } ?>"><?php _e('Progress Bar', 'bsf_rt_textdomain'); ?></a>
+        <a href="?page=bsf_rt&tab=bsf_rt_user_manual" class="nav-tab tb <?php if ($active_tab == 'bsf_rt_user_manual') {
+                        echo 'nav-tab-active';
+        } ?>"><?php _e('User Manual', 'bsf_rt_textdomain'); ?></a>
 </h2>
 
 <?php
@@ -39,6 +45,8 @@ if (isset($_GET["tab"])) {
            include 'bsf-rt-read-time-settings.php';
     } elseif ($_GET["tab"] == "bsf_rt_progress_bar_settings") {
            include 'bsf-rt-progress-bar-settings.php';
+    } elseif ($_GET["tab"] == "bsf_rt_user_manual") {
+           include 'bsf-rt-user-manual.php';
     }   
 }
 else
