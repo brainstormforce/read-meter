1f1ca2cf3a783aec65130af81636a6ec76e82b2f
Rajkiran Gajanan Bagal <rajkiranb@bsf.io>
2019-05-25 17:25:28 -0700

    changed the format of getting and updating options and also the code formatting general settings and loader file

--- classes/class-bsfrt-loader.php
+++ classes/class-bsfrt-loader.php
@@ -1,25 +1,25 @@
 <?php
 /**
- * BSF ReadTime Loader Doc comment
+ * BSF Read Meter Loader Doc comment
  *
  * PHP version 7
  *
  * @category PHP
- * @package  BSF ReadTime
- * @author   Display Name <username@ahemads.com>
+ * @package  Read Meter
+ * @author   Display Name <username@rajkiranb.com>
  * @license  http://brainstormforce.com
  * @link     http://brainstormforce.com
  */
 
 if (! class_exists('BSF_RT_Loader') ) :
     /**
-     * WCM Loader Doc comment
+     * Read Meter Loader Doc comment
      *
      * PHP version 7
      *
      * @category PHP
-     * @package  Google_Pagespeed_Insights_Portal
-     * @author   Display Name <username@ahemads.com>
+     * @package  Read Meter
+     * @author   Display Name <username@rajkiranb.com>
      * @license  http://brainstormforce.com
      * @link     http://brainstormforce.com
      */
@@ -28,9 +28,13 @@ if (! class_exists('BSF_RT_Loader') ) :
          * Constructor
          */
         public function __construct() {
+
             include BSF_RT_ABSPATH.'includes/bsf-rt-page.php'; 
+
             add_action('init', array($this, 'bsfrt_pluginStyle'));
+
             add_action('init', array($this, 'bsfrt_pluginScript'));
+
             register_deactivation_hook(BSF_RT_PATH, array($this,'bsf_rt_remove_data'));
            
         }         
@@ -41,7 +45,9 @@ if (! class_exists('BSF_RT_Loader') ) :
          * @return void
          */
         public function bsfrt_pluginStyle() { 
+
              wp_enqueue_style('wp-color-picker');
+
              wp_enqueue_style('customstyle', BSF_RT_PLUGIN_URL.'/assets/css/styles.css');
         }
          /**
@@ -51,13 +57,18 @@ if (! class_exists('BSF_RT_Loader') ) :
           * @return void
           */
         public function bsfrt_pluginScript ($hook) { 
+
              wp_enqueue_script('customscript', BSF_RT_PLUGIN_URL.'/assets/js/bsf-rt.js');
+
              wp_enqueue_script('colorpickerscript', BSF_RT_PLUGIN_URL.'/assets/js/color-picker.js',array('jquery','wp-color-picker'), null, true);
             }
 
         public function bsf_rt_remove_data () {
+
             delete_option('bsf_rt_general_settings');
+
             delete_option('bsf_rt_progress_bar_settings');
+            
             delete_option('bsf_rt_read_time_settings');
             }
             
--- includes/bsf-rt-general-settings.php
+++ includes/bsf-rt-general-settings.php
@@ -1,140 +1,200 @@
 <?php
-$options=get_option('bsf_rt_general_settings');
-?>
-<div class="bsf_rt_global_settings" id="bsf_rt_global_settings">
-  <form action="" method="post" name="bsf_rt_settings_form">
-    <table class="form-table" > 
-      <tr>
-        <h3> General Settings </h3>
-      </tr>
-      <p class="description">
-        Control the core settings of a read meter, e.g. the average count of words that humans can read in a minute & allow a read meter on particular post types, etc.
-      </p>  
-      
-      <tr>
-        <th scope="row">
-          <label for="WordsPerMinute">Words Per Minute :</label>
-        </th>
-        <td>
-          <input type="number" required name="bsf_rt_words_per_minute" placeholder="275" value="<?php  echo $options['bsf_rt_words_per_minute'];?>" class="small-text">
-        </td>
-      </tr>
+$options = get_option('bsf_rt_general_settings');
 
-      <tr>
-        <th scope="row">
-          <label for="SelectPostTypes">Select Post Types :</label>
-        </th>
-        <td class="post_type_name">
-            <?php
-            $args = array(
+$bsf_rt_words_per_minute = '';
+
+$bsf_rt_post_types = array();
+
+$bsf_rt_include_images = '';
+
+$bsf_rt_include_comments = '';
+
+if (isset($options['bsf_rt_words_per_minute'])) {
+
+  $bsf_rt_words_per_minute = $options['bsf_rt_words_per_minute'];
+}
+
+if (isset($options['bsf_rt_post_types'])) {
+
+  $bsf_rt_post_types = $options['bsf_rt_post_types'];
+}
+
+if (isset($options['bsf_rt_include_images'])) {
+
+  $bsf_rt_include_images = $options['bsf_rt_include_images'];
+}
+
+if (isset($options['bsf_rt_include_comments'])) {
+
+  $bsf_rt_include_comments = $options['bsf_rt_include_comments'];
+}
+
+$args = array(
             'public'   => true,
             
             );
            
-            $exclude=array('attachment','elementor_library','Media','My Templates');
+$exclude=array('attachment','elementor_library','Media','My Templates');
+?>
+<div class="bsf_rt_global_settings" id="bsf_rt_global_settings">
+  <form action="" method="post" name="bsf_rt_settings_form">
+    <table class="form-table" > 
+        <tr>
+          
+          <h3> General Settings </h3>
+        </tr>
+          <p class="description">
 
-            foreach ( get_post_types($args, 'objects') as $post_type ) {
+              Control the core settings of a read meter, e.g. the average count of words that humans can read in a minute & allow a read meter on particular post types, etc.
+          </p>  
+        
+        <tr>
+          <th scope="row">
+            <label for="WordsPerMinute">Words Per Minute :</label>
+          </th>
+          <td>
+            <input type="number" required name="bsf_rt_words_per_minute" placeholder="275" value="<?php  echo $bsf_rt_words_per_minute; ?>" class="small-text">
+          </td>
+        </tr>
 
-                if (in_array($post_type->labels->name, $exclude)  ) {
-                    continue;
-                } 
-                if ($options['bsf_rt_post_types'] !== 'post') {
-                if (isset($options['bsf_rt_post_types'])) {
-                    if (in_array($post_type->name, $options['bsf_rt_post_types'])) {
+        <tr>
+            <th scope="row">
+              <label for="SelectPostTypes">Select Post Types :</label>
+            </th>
+            <td class="post_type_name">
+                  
+                  <?php
+                
+                foreach ( get_post_types($args, 'objects') as $post_type ) {
+
+                      if (in_array($post_type->labels->name, $exclude)  ) {
+
+                          continue;
+                      } 
+                      if ($bsf_rt_post_types !== 'post') {
+                        if (isset($bsf_rt_post_types)) {
+                          if (in_array($post_type->name, $bsf_rt_post_types)) {
+                             echo'<label for="ForPostType">
+                             <input type="checkbox" checked name="posts[]" value="'.$post_type->name.'">
+                             '.$post_type->labels->name.'</label><br> ';
+                              } else {
+                                  echo'<label for="ForPostType">
+                             <input type="checkbox"  name="posts[]" value="'.$post_type->name.'">
+                             '.$post_type->labels->name.'</label><br> ';
+                              }
+                          } else {
+                              echo'<label for="ForPostType">
+                             <input type="checkbox"  name="posts[]" value="'.$post_type->name.'">
+                             '.$post_type->labels->name.'</label><br> ';
+                          }
+                      } else {
+                      if ($post_type->name == 'post') {
                         echo'<label for="ForPostType">
-                   <input type="checkbox" checked name="posts[]" value="'.$post_type->name.'">
-                   '.$post_type->labels->name.'</label><br> ';
-                    } else {
-                        echo'<label for="ForPostType">
-                   <input type="checkbox"  name="posts[]" value="'.$post_type->name.'">
-                   '.$post_type->labels->name.'</label><br> ';
-                    }
-                } else {
-                    echo'<label for="ForPostType">
-                   <input type="checkbox"  name="posts[]" value="'.$post_type->name.'">
-                   '.$post_type->labels->name.'</label><br> ';
+                         <input type="checkbox" checked name="posts[]" value="'.$post_type->name.'">
+                         '.$post_type->labels->name.'</label><br> ';
+                      }
+                       echo'<label for="ForPostType">
+                         <input type="checkbox"  name="posts[]" value="'.$post_type->name.'">
+                         '.$post_type->labels->name.'</label><br> ';
+                          }
                 }
+                   ?>
+           </td>
+        </tr>
+        <tr>
+          <th scope="row">
+
+            <label for="IncludeComments">Include Comments :</label>
+          </th>
+          <td>
+              <?php if (isset($bsf_rt_include_comments) && $bsf_rt_include_comments == 'yes') {
+                 echo '<input type="checkbox" checked name="bsf_rt_include_comments" value="yes">';
               } else {
-                if ($post_type->name == 'post') {
-                  echo'<label for="ForPostType">
-                   <input type="checkbox" checked name="posts[]" value="'.$post_type->name.'">
-                   '.$post_type->labels->name.'</label><br> ';
-                }
-                 echo'<label for="ForPostType">
-                   <input type="checkbox"  name="posts[]" value="'.$post_type->name.'">
-                   '.$post_type->labels->name.'</label><br> ';
-                    }
-                }
+                echo '<input type="checkbox" name="bsf_rt_include_comments" value="yes">';
+              }
              ?>
-         
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">
-         <label for="IncludeComments">Include Comments :</label>
-        </th>
-        <td>
-          <?php if (isset($options['bsf_rt_include_comments']) && $options['bsf_rt_include_comments'] == 'yes') {
-             echo '<input type="checkbox" checked name="bsf_rt_include_comments" value="yes">';
-          } else {
-            echo '<input type="checkbox" name="bsf_rt_include_comments" value="yes">';
-          }
-         ?>
-          <p class="description">                    
-         Check if you want to count the comments in the Reading time.
-          </p>  
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">
-         <label for="IncludeImages">Include Images :</label>
-        </th>
-        <td>
-          <?php if (isset($options['bsf_rt_include_images']) && $options['bsf_rt_include_images'] == 'yes') {
-             echo '<input type="checkbox" checked name="bsf_rt_include_images" value="yes">';
-          } else {
-            echo '<input type="checkbox" name="bsf_rt_include_images" value="yes">';
-          }
-         ?>
-          <p class="description">                    
-         Check if you want to count the Images in the Reading time.
-          </p>  
-        </td>
-      </tr>
-</table>
-<table class="form-table">
+              <p class="description">
+
+                 Check if you want to count the comments in the Reading time.
+              </p>  
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">
+
+             <label for="IncludeImages">Include Images :</label>
+          </th>
+          <td>
+            <?php if (isset($bsf_rt_include_images) && $bsf_rt_include_images == 'yes') {
+
+               echo '<input type="checkbox" checked name="bsf_rt_include_images" value="yes">';
+            } else {
+
+              echo '<input type="checkbox" name="bsf_rt_include_images" value="yes">';
+            }
+           ?>
+            <p class="description">   
+
+               Check if you want to count the Images in the Reading time.
+            </p>  
+          </td>
+        </tr>
+    </table>
+    <table class="form-table">
        <tr>
           <th>
-          <input type="submit" value="Save" class="bt button button-primary" name="submit">
+
+            <input type="submit" value="Save" class="bt button button-primary" name="submit">
           </th>
        </tr>
-  </table>
+    </table>
   </form>
 </div>
 
 <?php
 if (isset($_POST['submit'])) {
 
-$bsf_rt_words_per_minute=$_POST['bsf_rt_words_per_minute'];
-$bsf_rt_post_types=$_POST['posts'];
-if (isset($_POST['bsf_rt_include_images'])) {
-$bsf_rt_include_images=$_POST['bsf_rt_include_images'];
-} else {
-  $bsf_rt_include_images = '';
-}
-if (isset($_POST['bsf_rt_include_comments'])) {
-$bsf_rt_include_comments=$_POST['bsf_rt_include_comments'];
-} else {
-  $bsf_rt_include_comments = '';
-}
+    $bsf_rt_words_per_minute=$_POST['bsf_rt_words_per_minute'];
+     if (isset($_POST['bsf_rt_words_per_minute'])) {
 
-$update_options = array(
-  'bsf_rt_words_per_minute'   => $bsf_rt_words_per_minute,
-  'bsf_rt_post_types'     => $bsf_rt_post_types,
-  'bsf_rt_include_comments' => $bsf_rt_include_comments,
-  'bsf_rt_include_images' => $bsf_rt_include_images,
+      $bsf_rt_words_per_minute=$_POST['bsf_rt_words_per_minute'];
 
-);
-update_option('bsf_rt_general_settings', $update_options);
-echo '<meta http-equiv="refresh" content="0.1" />';
+    } else {
+
+      $bsf_rt_words_per_minute = '';
+    }
+    
+    if (isset($_POST['posts'])) {
+
+     $bsf_rt_post_types=$_POST['posts'];
+
+    } else {
+
+      $bsf_rt_post_types = array();
+    }
+    if (isset($_POST['bsf_rt_include_images'])) {
+
+      $bsf_rt_include_images=$_POST['bsf_rt_include_images'];
+
+    } else {
+
+      $bsf_rt_include_images = '';
+    }
+    if (isset($_POST['bsf_rt_include_comments'])) {
+
+      $bsf_rt_include_comments=$_POST['bsf_rt_include_comments'];
+    } else {
+
+      $bsf_rt_include_comments = '';
+    }
+
+    $update_options = array(
+      'bsf_rt_words_per_minute'   => $bsf_rt_words_per_minute,
+      'bsf_rt_post_types'     => $bsf_rt_post_types,
+      'bsf_rt_include_comments' => $bsf_rt_include_comments,
+      'bsf_rt_include_images' => $bsf_rt_include_images,
+
+    );
+    update_option('bsf_rt_general_settings', $update_options);
+    echo '<meta http-equiv="refresh" content="0.1" />';
 }
