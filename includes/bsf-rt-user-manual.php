af1dc35ad5b6abfaf912e368c8b2cdbb0da864e8
Rajkiran Gajanan Bagal <rajkiranb@bsf.io>
2019-06-07 15:49:40 +0530

    Changes Getting started tab's content as per Anjali Mam

--- includes/bsf-rt-user-manual.php
+++ includes/bsf-rt-user-manual.php
@@ -2,13 +2,12 @@
 	<br><label class="bsf_rt_page_title" for="howtouse">
 		<?php _e(
 			'How to Use? </label><br><br>
-	<b>Step 1</b> : Go to the <i>General Settings</i> Tab . Select post types on which you want to display the Read Time and Progress bar. Select Words Per Minute and set other options if required. <br><br>
-	<b>Step 2</b> : Go to the <i>Read Time</i> Tab  and select target pages where you wish to display it. Set position, prefix, postfix, and other styling options. <br><br>
-	<b>Step 3</b> : Go to the <i>Progress Bar</i> Tab and select the position, colors, and thickness of the progress bar as per your need. <br><br>
-	<b>Step 4</b> : You are done! Visit a Single Post/Page/CPT to see Read Time and Progress bar.  <br><br><br>
+	<b>Step 1</b> :Under the <i>General Settings</i> Tab, select post types to display the Read Time and Progress bar. Select Words Per Minute and set other options if required. <br><br>
+	<b>Step 2</b> : Go to the <i>Read Time</i> Tab, and select target pages. Set position, prefix, postfix, and other styling options. <br><br>
+	<b>Step 3</b> : Go to the <i>Progress Bar</i> Tab, and select the position, colors, and thickness of the progress bar as per your need. <br><br>
+	<b>Step 4</b> :That'."'".'s it! Visit Post/Page to see results.  <br><br><br>
 	<b> Shortcode : [read_meter]</b> <br><br>
 		You can also display the reading time wherever you want by using this shortcode , You just need to copy it and paste it in any content of Post or Page.',
 			'read-meter'
 		); ?>
 </div>
- 
